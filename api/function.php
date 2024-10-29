7ece52bcb0b36f744d224fd652feb69120eea106
Andre-231401040 <andrelim865@gmail.com>
2024-10-29 15:11:39 +0700

    Menambah port di function.php

--- api/function.php
+++ api/function.php
@@ -1,11 +1,11 @@
 <?php 
 $host = $_ENV["PG_HOST"];
 $user = $_ENV["PG_USER"];
+$port = $_ENV["PG_PORT"];
 $password = $_ENV["PG_PASSWORD"];
 $dbname = $_ENV["PG_DB"];
-$con = pg_connect("host=$host user=$user password=$password dbname=$dbname");
+$con = pg_connect("host=$host port=$port user=$user password=$password dbname=$dbname");
 if(!$con){
     die("Koneksi ke Database Gagal.");
 }
-
 ?>
