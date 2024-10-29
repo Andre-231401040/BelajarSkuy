937e3cc2205b3b0e75fc1c238fb3d63375c14104
Andre-231401040 <andrelim865@gmail.com>
2024-10-29 15:17:58 +0700

    Mengedit vercel.json dan function.php

--- api/function.php
+++ api/function.php
@@ -1,9 +1,9 @@
 <?php 
-$host = $_ENV["PG_HOST"];
-$user = $_ENV["PG_USER"];
-$port = $_ENV["PG_PORT"];
-$password = $_ENV["PG_PASSWORD"];
-$dbname = $_ENV["PG_DB"];
+$host = "ep-damp-waterfall-a13tat59-pooler.ap-southeast-1.aws.neon.tech";
+$user = "default";
+$port = "5432";
+$password = "PfrRKpVY73zU";
+$dbname = "verceldb";
 $con = pg_connect("host=$host port=$port user=$user password=$password dbname=$dbname");
 if(!$con){
     die("Koneksi ke Database Gagal.");
